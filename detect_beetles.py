761fb1defb4a18f10b2f50e8a4ee40ca842de414
Bostame Md Bayazid <md.bostame@uni-potsdam.de>
2022-01-20 09:57:49 +0100

    handler added yolov3

--- detect_beetles.py
+++ detect_beetles.py
@@ -7,7 +7,7 @@
 #
 #================================================================
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import cv2
 import numpy as np
 import random
@@ -27,4 +27,4 @@ while True:
     yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
     yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}") # use keras weights
 
-    detect_image(yolo, image_path, "mnist_test.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+    detect_image(yolo, image_path, "DSC07364_029.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
